e2ad3a476100f2b9b3e7cf55c69de92168f95632
daretogo <raaronjohnson@gmail.com>
2023-11-15 13:44:36 -0500

    this weeks class plan

---
+++ Week10/ClassExample.py
@@ -0,0 +1,37 @@
+# ClassExample.py
+
+# Welcome to today's coding class. In addition to our usual week-by-week folders, 
+# we have introduced a new folder in our class GitHub repository named "student_folders".
+# This folder contains a sub-folder for each student in the class.
+
+# Inside each student's folder, you will find the answer they provided for last week's homework. 
+# This includes a link to a recipe and an explanation of how the food relates to the holiday they were assigned.
+
+# Today's goal is to organize this data into a structured format. We will create variables 
+# that store ordered lists, each representing key information about the holiday and the associated food.
+
+# Let's think about the key items or data points we want for each holiday/food combination.
+# We need to know the name of the holiday, the student who researched it, the date of the holiday, 
+# the URL of the recipe, and the student's explanation of the food's relevance to the holiday.
+
+# We can represent this data using an ordered list stored in a variable. Here's an example:
+holiday_data = ["holiday_name", "student_name", "holiday_date", "recipe_URL", "relevance_explanation"]
+
+# During today's class, you will have some time to review each person's file in the repository.
+# Your task is to create a series of variables like 'holiday_data' for each entry. 
+# Store this information in a new file within your student folder in the repository.
+
+### 15 Minutes Worktime ### 
+
+# Once you have created these data sets, we will proceed to the next step, which involves 
+# committing your changes to the repository. For this, we need to ensure each of you has a GitHub account and can commit to the creeksidecoding repo from your raspberry pi's. 
+
+
+# If you do not have a GitHub account yet, we will guide you through the process of creating one.
+# Afterwards, each student will be added as a collaborator to the class repository. 
+# This will allow you to push your changes and make your contributions visible to everyone.
+
+# Remember, the skills you are learning today are not just about coding, 
+# but also about using tools like GitHub for collaboration and version control – essential skills for any developer.
+
+# Good luck, and let's start coding!
